76b1c225376f60c6f961d0e78fb69fb253785b8d
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2026-02-20 15:45:04 +0100

    org/project/branch selector divider

--- apps/studio/components/layouts/ProjectLayout/NavigationBar/ProjectBranchSelectorSheet.tsx
+++ apps/studio/components/layouts/ProjectLayout/NavigationBar/ProjectBranchSelectorSheet.tsx
@@ -103,15 +103,16 @@ export function ProjectBranchSelectorSheet({
               tabs.length === 3 && 'grid-cols-3'
             )}
           >
-            {tabs.map(({ value, label, icon: Icon }) => {
+            {tabs.map(({ value, label, icon: Icon }, index) => {
               const isBranch = value === 'branch'
               const isProductionBranch = isBranch && isMainBranch
+              const isLast = index === tabs.length - 1
               return (
                 <TabsTrigger_Shadcn_
                   key={value}
                   value={value}
                   className={cn(
-                    'text-xs flex flex-col items-center gap-1.5 px-4 py-3 data-[state=active]:bg-surface-200 data-[state=active]:border-foreground-light border-b border-r !border-r-muted last:border-r-0',
+                    'group relative text-xs flex flex-col items-center gap-1.5 px-4 py-3 data-[state=active]:bg-surface-200 data-[state=active]:border-foreground-light border-b duration-0',
                     isProductionBranch &&
                       'text-warning data-[state=active]:text-warning hover:text-warning hover:opacity-70'
                   )}
@@ -120,6 +121,27 @@ export function ProjectBranchSelectorSheet({
                   <span className="truncate max-w-full text-xs leading-tight" title={label}>
                     {label}
                   </span>
+
+                  {!isLast && (
+                    <svg
+                      aria-hidden="true"
+                      width="100%"
+                      height="100%"
+                      viewBox="0 0 6 63"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="absolute duration-0 bottom-0 top-0 -right-2 w-3 h-full z-10"
+                    >
+                      <path
+                        d="M5.49365 31L0.493652 0V62L5.49365 31Z"
+                        className="fill-[hsl(var(--background-dash-sidebar))] group-data-[state=active]:fill-[hsl(var(--background-surface-200))]"
+                      />
+                      <path
+                        d="M0.493652 0L5.49365 31L0.493652 62"
+                        stroke="hsl(var(--border-default))"
+                      />
+                    </svg>
+                  )}
                 </TabsTrigger_Shadcn_>
               )
             })}
